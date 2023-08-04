3b2950ab6f900f5beeecc121934b99849a0164f2
raphaelfretter <fretterr@gmail.com>
2023-08-04 15:04:45 -0600

    styles fonts and routing

---
+++ scripts/router.js
@@ -0,0 +1,70 @@
+document.addEventListener("click", e => {
+    const { target } = e;
+    if (!target.matches("nav a")) {
+        return;
+    }
+    e.preventDefault();
+    route();
+});
+
+const routes = {
+    404: {
+        template: "/templates/404.html",
+        title: "404",
+        description: "Page not found",
+    },
+    "/": {
+        template: "/",
+        title: "Home",
+        description: "This is the home page",
+    },
+    "/blog": {
+        template: "/src/blog.html",
+        title: "Blog",
+        description: "This the blog page",
+    },
+    "/contact": {
+        template: "/src/contact.html",
+        title: "Contact",
+        description: "This is the contact page",
+    },
+    "/projects": {
+        template: "/src/projects.html",
+        title: "Projects",
+        description: "This is the projects page",
+    }
+}
+
+const route = event => {
+    event = event || window.event;
+    event.preventDefault();
+
+    window.history.pushState({}, "", event.target.href);
+    locationHandler();
+}
+
+const locationHandler = async () => {
+    const location = window.location.pathname;
+
+    if (location.length == 0) {
+        location = "/";
+    }
+
+    const route = routes[location] || routes["404"];
+
+    const html = await fetch(route.template).then((response) => response.text());
+
+    document.getElementsByClassName("content").innerHTML = html;
+
+    document.title = route.title;
+
+    document
+        .querySelector('meta[name="description"]')
+        .setAttribute("content", route.description);
+}
+
+window.onpopstate = locationHandler;
+
+window.route = route;
+
+locationHandler();
