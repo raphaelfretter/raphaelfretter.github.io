e82f5d26a6eeb04d081468685a1d664396208001
raphaelfretter <fretterr@gmail.com>
2023-08-04 15:46:01 -0600

    working router

--- scripts/router.js
+++ scripts/router.js
@@ -1,6 +1,7 @@
-document.addEventListener("click", e => {
+// create document click that watches the nav links only
+document.addEventListener("click", (e) => {
     const { target } = e;
-    if (!target.matches("nav a")) {
+    if (!target.matches("#navbar a")) {
         return;
     }
     e.preventDefault();
@@ -14,7 +15,7 @@ const routes = {
         description: "Page not found",
     },
     "/": {
-        template: "/",
+        template: "/src/home.html",
         title: "Home",
         description: "This is the home page",
     },
@@ -35,37 +36,39 @@ const routes = {
     }
 }
 
-const route = event => {
-    event = event || window.event;
+const route = (event) => {
+    event = event || window.event; // get window.event if event argument not provided
     event.preventDefault();
-
+    // window.history.pushState(state, unused, target link);
     window.history.pushState({}, "", event.target.href);
     locationHandler();
-}
+};
 
 const locationHandler = async () => {
-    const location = window.location.pathname;
-
+    const location = window.location.pathname; // get the url path
+    // if the path length is 0, set it to primary page route
     if (location.length == 0) {
         location = "/";
     }
-
+    // get the route object from the urlRoutes object
     const route = routes[location] || routes["404"];
-
+    // get the html from the template
     const html = await fetch(route.template).then((response) => response.text());
-    console.log(html);
-
-    document.getElementsByClassName("content").innerHTML = html;
-
+    // set the content of the content div to the html
+    document.getElementById("content").innerHTML = html;
+    // set the title of the document to the title of the route
     document.title = route.title;
+    // set the description of the document to the description of the route
+    // const meta = document.getElementsByTagName("meta");
 
     document
         .querySelector('meta[name="description"]')
-        .setAttribute("content", route.description);
-}
+        .content = route.description;
+};
 
+// add an event listener to the window that watches for url changes
 window.onpopstate = locationHandler;
-
+// call the urlLocationHandler function to handle the initial url
 window.route = route;
-
+// call the urlLocationHandler function to handle the initial url
 locationHandler();
